f3089366d2fdd07b80febe2433a265e84b7b89f0
Ohm5656 <natdanailunaha@gmail.com>
2025-11-15 20:08:41 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -45,7 +45,12 @@ app = FastAPI(title="GlowbieBell Backend", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://aishincarefrontend-production.up.railway.app",
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
